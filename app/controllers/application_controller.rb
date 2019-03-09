1ca7b1e4264aadabbcf4c1810cd660518f5788bf
Taichi Kanaya <taichi.kanaya@attakait.com>
2019-03-09 20:43:11 +0900

    Basic認証をstaging環境にかける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
-
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "staging"
   private
   def require_login
     unless session[:id]
