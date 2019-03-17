46fbe6815fbd2dd00d5f89ccae96de05b152d385
Taichi Kanaya <taichi.kanaya@attakait.com>
2019-03-17 10:04:57 +0900

    メール送信対応

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,8 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'localhost',
-  domain: 'attakait.com',
-  port: 1025,
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
+  port: 587,
+  user_name: 'k.t.sz1989@gmail.com',
+  password: 'bshBSH100cp',
   authentication: 'plain',
   enable_starttls_auto: true
 }
