18d769de0d408ca7745e95c1e616eed384e0ae29
Taichi Kanaya <taichi.kanaya@attakait.com>
2019-03-17 15:49:28 +0900

    不要ファイル削除、退会処理実装

--- app/assets/javascripts/ctg_share.coffee
+++ app/assets/javascripts/ctg_share.coffee
@@ -1,3 +1,11 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+options = {
+  now: moment().add(-39,'year').toDate()
+  max: moment().toDate(),
+  selectYears: true
+}
+
+# 生年月日設定
+$('#birthday').pickadate(options)
--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -1,7 +1,24 @@
 class UnsubscribeController < ApplicationController
   def index
   end
+
   def execute
+    ActiveRecord::Base.transaction do
+      # user
+      User.where("id = ?", session[:id]).delete_all
+      
+      # user_access_categories
+      UserAccessCategory.where("user_id = ?", session[:id]).delete_all
+      UserAccessCategory.where("created_user_id = ?", session[:id]).delete_all
+      
+      # questions
+      Question.where("created_user_id = ?", session[:id]).delete_all
+
+      # Categories
+      Category.where("created_user_id = ?", session[:id]).delete_all
+      # user
+      User.where("created_user_id = ?", session[:id]).delete_all
+    end
     redirect_to :controller => '/unsubscribe_comp'
   end
 end
