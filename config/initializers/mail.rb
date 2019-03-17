b1426ea0d6cedf66f57deda1fb8c663b0a39a0ba
Taichi Kanaya <taichi.kanaya@attakait.com>
2019-03-17 11:36:04 +0900

    Revert "attakait.comでのメール送信設定"
    This reverts commit af44d986b4a63f0dab21ea370928230fcc408ca0.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.office365.com',
-  domain: 'attakait.com',
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
   port: 587,
-  user_name: 'taichi.kanaya@attakait.com',
-  password: 'bshBSH100cp_1989',
+  user_name: 'jimonjitou.attakait@gmail.com',
+  password: 'uituodcypvfklhfm',
   authentication: 'plain',
   enable_starttls_auto: true
 }
